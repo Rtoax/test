7de5ee536d4dc01a29e29b2d72021b5ac4bc12a1
rtoax <2386499836@qq.com>
2021-04-07 12:53:57 +0800

    linux 驱动

--- linux/driver/cdev/book2-the-linux-kernel-module-programming-guide-EN/Talking-to-Device-File/chardev.c
+++ linux/driver/cdev/book2-the-linux-kernel-module-programming-guide-EN/Talking-to-Device-File/chardev.c
@@ -15,6 +15,7 @@ static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+
 #define SUCCESS 0
 #define DEVICE_NAME "chardev" /* Dev name as it appears in /proc/devices */
 #define BUF_LEN 80 /* Max length of the message from the device */
@@ -26,6 +27,7 @@ static int Device_Open = 0; /* Is device open?
 							   * Used to prevent multiple access to device */
 static char msg[BUF_LEN]; /* The msg the device will give when asked */
 static char *msg_Ptr;
+
 static struct file_operations fops = {
 	.read = device_read,
 	.write = device_write,

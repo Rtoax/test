44477140a5f363c9a1d2d179376ad6232f74fff2
rtoax <2386499836@qq.com>
2021-04-14 16:19:23 +0800

    __atomic_xxx(v, __ATOMIC_ACQUIRE)

--- linux/api/rdtsc-4.c
+++ linux/api/rdtsc-4.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdint.h>
 
 #if defined __x86_64__
 static inline uint64_t __tsc(void)
@@ -17,3 +19,13 @@ static inline uint64_t __tsc(void)
 
 #endif
 
+int main() {
+	uint64_t v1 = 0, v2 = 0;
+	printf("%lx - %lx\n", (0xffffULL << 48), ~(0xffffULL << 48));
+	while(1) {
+		v1 = __tsc() & ~(0xffffULL << 48);
+		sleep(1);
+		v2 = __tsc() & ~(0xffffULL << 48);
+		printf("%lx - %lx = %lx\n", v2, v1, v2 - v1);
+	}
+}

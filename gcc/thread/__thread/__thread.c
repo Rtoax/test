910dbd146ce3d9d30b9f74507faa0315769fc269
rtoax <2386499836@qq.com>
2021-05-20 18:13:30 +0800

    getcpu; sched_getcpu; __thread;

---
+++ gcc/thread/__thread/__thread.c
@@ -0,0 +1,76 @@
+//https://stackoverflow.com/questions/32245103/how-does-the-gcc-thread-work
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <syscall.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/sysinfo.h>
+#include <time.h>
+//#include <linux/getcpu.h>
+
+#define __USE_GNU
+#include <sched.h>
+#include <ctype.h>
+#include <string.h>
+
+
+pthread_key_t key;
+
+__thread int data __attribute__((tls_model("local-exec"))) = 3;
+
+struct log_struct {
+    FILE  * fp;
+    char filename[256];
+};
+
+struct thread_arg {
+    int i;
+};
+
+int get_by_key (void) {
+    return *(int*) (pthread_getspecific (key));
+}
+
+
+void* task_routine(void* parameters)
+{
+    struct thread_arg * arg = (struct thread_arg*)parameters;
+    
+    int i = arg->i;
+    while(i--)
+        data++;
+
+    printf("%ld: data = %d\n", pthread_self(), data);
+    pthread_exit(arg);
+}
+
+/* The main program. */
+int main ()
+{
+    int ncpu = sysconf (_SC_NPROCESSORS_ONLN);
+    int i;
+	pthread_t tids[4096];
+    int nr_threads = 1280;
+    
+    for(i=0; i<nr_threads;i++) {
+        struct thread_arg * arg = malloc(sizeof(struct thread_arg));
+        arg->i = i;
+	    pthread_create(&tids[i], NULL, &task_routine, (void*)arg);
+    }
+
+    for(i=0; i<nr_threads;i++) {
+        void *p = NULL;
+        
+        pthread_join(tids[i], &p);
+
+        free(p);
+    }
+
+    printf("threads exit.\n");
+    printf("%ld: data = %d\n", pthread_self(), data);
+	return 0;
+}
+

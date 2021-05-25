a3d0ae16f9167a121ff6637a8151648137e74024
rtoax <2386499836@qq.com>
2021-05-25 15:51:16 +0800

    pthread_key_create -> __nptl_deallocate_tsd

--- glibc/segmentation-fault/__nptl_deallocate_tsd/sane/main.c
+++ glibc/segmentation-fault/__nptl_deallocate_tsd/sane/main.c
@@ -1,10 +1,16 @@
 #include <assert.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <malloc.h>
 #include <sane/sane.h>
 
+#define PTHREAD_STACK_MIN	16384
+
 void* scan_thread(void *arg) {
+    
     SANE_Status status;
 
     status = sane_init(NULL, NULL);
@@ -22,9 +28,18 @@ void* scan_thread(void *arg) {
     sane_exit();
 }
 
-int main() {
-    
+int main()
+{    
 	pthread_t t;
+    pthread_attr_t attr;
+    void *stackAddr = NULL;
+    int paseSize = getpagesize();
+    size_t stacksize = paseSize*4;
+    
+    pthread_attr_init(&attr);
+    posix_memalign(&stackAddr, paseSize, stacksize);
+    pthread_attr_setstack(&attr, stackAddr, stacksize);
+    
 	pthread_create(&t, NULL, scan_thread, NULL);
     pthread_join(t, NULL);
     return 0;

a71bcf94a8a7cf5c1da03b305714965793de68f6
rtoax <2386499836@qq.com>
2021-05-27 14:14:03 +0800

    系统调用: membarrier; userfaultfd

--- c/glibc/sys/syscall/membarrier.h
+++ c/glibc/sys/syscall/membarrier.h
@@ -1,9 +1,32 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <linux/membarrier.h>
 
 // /usr/include/bits/syscall.h:784:#ifdef __NR_membarrier
 // /usr/include/bits/syscall.h:785:# define SYS_membarrier __NR_membarrier
 // /usr/include/SDL2/SDL_atomic.h:178:#define SDL_MemoryBarrierRelease()   __cpu_membarrier()
 // /usr/include/SDL2/SDL_atomic.h:179:#define SDL_MemoryBarrierAcquire()   __cpu_membarrier()
 
+enum membarrier_cmd {
+	MEMBARRIER_CMD_QUERY					= 0,
+	MEMBARRIER_CMD_GLOBAL					= (1 << 0),
+	MEMBARRIER_CMD_GLOBAL_EXPEDITED				= (1 << 1),
+	MEMBARRIER_CMD_REGISTER_GLOBAL_EXPEDITED		= (1 << 2),
+	MEMBARRIER_CMD_PRIVATE_EXPEDITED			= (1 << 3),
+	MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED		= (1 << 4),
+	MEMBARRIER_CMD_PRIVATE_EXPEDITED_SYNC_CORE		= (1 << 5),
+	MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED_SYNC_CORE	= (1 << 6),
+
+	/* Alias for header backward compatibility. */
+	MEMBARRIER_CMD_SHARED			= MEMBARRIER_CMD_GLOBAL,
+};
+    
+enum membarrier_cmd_flag {
+    MEMBARRIER_CMD_FLAG_CPU     = (1 << 0),
+};
+
+
+int membarrier(int cmd, unsigned int flags, int cpu_id);
 
 
 /**

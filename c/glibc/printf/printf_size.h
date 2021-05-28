9a02b995fdaac6bfcc61bc12f091add86e34c2a3
rtoax <2386499836@qq.com>
2021-05-28 15:19:19 +0800

    glibc: parse_printf_format

---
+++ c/glibc/printf/printf_size.h
@@ -0,0 +1,7 @@
+#include <printf.h>
+
+
+int printf_size (FILE *__restrict __fp,
+			const struct printf_info *__info,
+			const void *const *__restrict __args);
+
---
+++ c/glibc/printf/printf_size_info.h
@@ -0,0 +1,5 @@
+#include <printf.h>
+
+extern int printf_size_info (const struct printf_info *__restrict
+			     __info, size_t __n, int *__restrict __argtypes);
+

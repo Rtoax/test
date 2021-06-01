60b8bf02d175714e7cd974992c5e317609aeb543
rtoax <2386499836@qq.com>
2021-06-01 09:19:11 +0800

    bpftrace 网络工具

--- bpf/bpftrace/study/one-liners/syscall-count.sh
+++ bpf/bpftrace/study/one-liners/syscall-count.sh
@@ -2,3 +2,6 @@
 
 # Syscall counts by process
 sudo bpftrace -e 'tracepoint:raw_syscalls:sys_enter { @[comm] = count(); }'
+
+# 应用程序正在使用poll 系统调用占用的文件描述符数量
+bpftrace -e 'tracepoint:syscalls:sys_enter_poll { @[comm, args->nfds] = count(); }'
